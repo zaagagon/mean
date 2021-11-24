defcce6a2f2018900541bb514541de177066a4d0
Alejandro Buendia Arango <zaagagon@gamail.com>
2021-11-24 18:30:51 -0500

    volviendo 2021

---
+++ server_express/apps.js
@@ -0,0 +1,34 @@
+const express    = require('express');
+const path= require('path');
+const   app= express();
+//app.listen(3000,()=>console.log('servidor en el puerto 3000'));
+
+
+
+//con estas tres lineas de codigo ya tenemos un servidor express funcionando
+
+//settings
+//configuramos el puerto
+app.set('port', process.env.PORT || 4000);
+//configuramos las vistas
+app.set('views',path.join(__dirname,'views'));
+//motor de plantillas ejs
+app.set('view engine','ejs');
+
+//middlewares
+//creamos una funcion flecha
+app.use((req,res,next)=>{
+    console.log(`${req.url}-${req.method}`);
+    next;//que pueda continuar
+}
+
+);
+
+//routes
+
+//static files
+
+//start the server
+app.listen(app.get('port'),()=>{
+    console.log('servidor en el puerto',app.get('port'))
+});
---
+++ server_express/routes/index.js
@@ -0,0 +1,4 @@
+const express = require ('express');
+const router = express.Router();//utilizamos el modulo express y metodo Router
+//con estas lineas de codigo ya tenemos un enrutador
+module.exports= router();
